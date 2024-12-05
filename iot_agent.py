95662f249a69a4511551ff7341d0a1fd1db76350
root <root@agentstack.us-central1-c.c.ivory-link-352701.internal>
2024-12-05 04:01:21 +0000

    add code and template
    add agent code and prompt template

---
+++ iot_agent.py
@@ -0,0 +1,53 @@
+import base64
+import requests
+import sys
+import time
+from github import Github
+
+
+token = "TOKEN"
+
+url = "http://localhost:11434/api/generate"
+
+f = open("agent_prompt_template.txt", "r")
+
+prompt_template = f.read()
+
+if len(sys.argv) < 2:
+    print("user prompt is required")
+    exit()
+
+user_prompt = sys.argv[1]
+
+prompt = prompt_template.replace("{prompt}", user_prompt)
+
+print("\n\nuser input:")
+print(user_prompt)
+
+
+print("\n\nmodel prompt:")
+print(prompt)
+
+
+data = {
+    "model": "llama3.2",
+    "prompt": prompt,
+    "stream": False
+}
+
+response = requests.post(url, json=data)
+model_response = response.json()["response"]
+print("\n\nmodel response:")
+print(model_response)
+
+
+print("\n\nnow agent will post the content to github...")
+
+print("\n\nvisit https://github.com/ChaoyiHuang/iot_messages...to see what happened")
+
+
+g = Github(token)
+repo = g.get_repo("ChaoyiHuang/iot_messages")
+cur_time = str(time.time_ns())
+upload_file = user_prompt + cur_time + ".json"
+repo.create_file(upload_file, 'upload new model response' + cur_time, model_response, branch='main')
